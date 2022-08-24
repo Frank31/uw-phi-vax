3ffb86d7b42abb93ac917dd665f7b2101fd369af
Jacob Armitage <jra162@uw.edu>
2022-08-24 16:49:02 -0700

    Update Heat_Maps.R

--- Visualizations/Heat_Maps.R
+++ Visualizations/Heat_Maps.R
@@ -5,13 +5,15 @@ library(tidyverse)
 
 #read in whole dataset
 df <- readRDS("/Users/ziva/R Projects/uw-phi-vax/aim_2/11_index_results.RDS")
+#Convert year from numeric into factor
+df <- df %>% mutate(year = as.factor(year))
 
 #Just 2019 data
 df_2019 <- df %>% filter(year ==2019)
-
+#Only North Africa and Middle East 
 df_NorthAM <- df %>% filter(region=="North Africa and Middle East")
 
-#Making plot
+#Making plot of Index Value changing over time for NorthAM Region
 ggplot(df_NorthAM,aes(x=year,y=location, fill=result))+
   #add border white colour of line thickness 0.25
   geom_tile(colour="white", size=0.10)+
@@ -22,9 +24,26 @@ ggplot(df_NorthAM,aes(x=year,y=location, fill=result))+
   #coord_fixed()+
   scale_fill_gradient(low = "#C6DBEF", high = "#08306B")+
   guides(fill=guide_legend(title="Index Value"))+
-  scale_y_discrete(expand=c(0, 0))
-  
-  
-  
-#Scale x discrete removes white space on left and right but also removes x label, example used breaks but when i do it, they still dissappear, may need to make year a factor?
+  scale_y_discrete(expand=c(0,0))+
+  scale_x_discrete(expand=c(0,0))
+ 
+#Remove some Columns that are not needed 
+df_simple_2019 <- df_2019[ , -c(2:7)]
 
+#Convert Data to Long format
+df_long_2019 <- df_simple_2019 %>% pivot_longer(!location,names_to = "component",values_to = "Value")
+
+#Make Heatmap of 2019 component variables
+ggplot(df_long_2019,aes(x=component,y=location, fill=Value))+
+  #add border white colour of line thickness 0.25
+  geom_tile(colour="white", size=0.10)+
+  #remove x and y axis labels
+  labs(x="", y="")+
+  theme_minimal(base_size=7)+
+  theme(axis.text.x = element_text(angle = 120))
+  #keeps it squares
+  #coord_fixed()+
+  # scale_fill_gradient(low = "#C6DBEF", high = "#08306B")+
+  # guides(fill=guide_legend(title="Index Value"))+
+  # scale_y_discrete(expand=c(0,0))+
+  # scale_x_discrete(expand=c(0,0))
