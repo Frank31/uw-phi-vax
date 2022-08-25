4559992f1e6980847fc79ae9211b097b4866ccc4
Jacob Armitage <jra162@uw.edu>
2022-08-24 17:42:37 -0700

    Update Heat_Maps.R

--- Visualizations/Heat_Maps.R
+++ Visualizations/Heat_Maps.R
@@ -8,8 +8,6 @@ df <- readRDS("/Users/ziva/R Projects/uw-phi-vax/aim_2/11_index_results.RDS")
 #Convert year from numeric into factor
 df <- df %>% mutate(year = as.factor(year))
 
-#Just 2019 data
-df_2019 <- df %>% filter(year ==2019)
 #Only North Africa and Middle East 
 df_NorthAM <- df %>% filter(region=="North Africa and Middle East")
 
@@ -26,6 +24,9 @@ ggplot(df_NorthAM,aes(x=year,y=location, fill=result))+
   guides(fill=guide_legend(title="Index Value"))+
   scale_y_discrete(expand=c(0,0))+
   scale_x_discrete(expand=c(0,0))
+
+#Just 2019 data
+df_2019 <- df %>% filter(year ==2019)
  
 #Remove some Columns that are not needed 
 df_simple_2019 <- df_2019[ , -c(2:7)]
@@ -39,11 +40,12 @@ ggplot(df_long_2019,aes(x=component,y=location, fill=Value))+
   geom_tile(colour="white", size=0.10)+
   #remove x and y axis labels
   labs(x="", y="")+
-  theme_minimal(base_size=7)+
-  theme(axis.text.x = element_text(angle = 120))
+  theme_minimal(base_size=3)+
+  theme(axis.text.x = element_text(angle = 45, hjust=1))
   #keeps it squares
-  #coord_fixed()+
+  #coord_fixed()
   # scale_fill_gradient(low = "#C6DBEF", high = "#08306B")+
   # guides(fill=guide_legend(title="Index Value"))+
   # scale_y_discrete(expand=c(0,0))+
   # scale_x_discrete(expand=c(0,0))
+
