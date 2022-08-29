a18cad48b252381084ac7cc56f8b3e35d23f458e
Jacob Armitage <jra162@uw.edu>
2022-08-29 16:37:50 -0700

    Update Heat_Maps.R

--- Visualizations/Heat_Maps.R
+++ Visualizations/Heat_Maps.R
@@ -1,9 +1,8 @@
 #Created by Jacob Armitage on Aug 23 2022
 #Intent is to create a heat maps for index and component variables
 
-
+#Required Libraries
 library(tidyverse)
-
 library(scico) #Diverging color scales for continuous data
 
 
@@ -12,21 +11,16 @@ df <- readRDS("/Users/ziva/R Projects/uw-phi-vax/aim_2/11_index_results.RDS")
 #Convert year from numeric into factor
 df <- df %>% mutate(year = as.factor(year))
 
-#Making Some Regional Subset
-#Only North Africa and Middle East 
-df_NorthAM <- df %>% filter(region=="North Africa and Middle East")
+#Automatically generating data frame for each region
+df_byregion <- split(df, df$region) #Puts them in list
+list2env(df_byregion, envir = .GlobalEnv)#Imports to environment
 
 
-#Making subsets by SDI level 2019
-#High
 
-#Medium
+#Making plot of Vaccine Index Value changing over time 
 
-#Low
-
-
-#Making plot of Index Value changing over time for NorthAM Region as example
-ggplot(df_NorthAM,aes(x=year,y=location, fill=result))+
+#Specific region as example
+ggplot(`Eastern Sub-Saharan Africa`,aes(x=year,y=location, fill=result))+
   #add border white color of line thickness 0.25
   geom_tile(colour="white", size=0.10)+
   #remove x and y axis labels
@@ -39,8 +33,15 @@ ggplot(df_NorthAM,aes(x=year,y=location, fill=result))+
   scale_y_discrete(expand=c(0,0))+
   scale_x_discrete(expand=c(0,0))+
   #keeps it squares
-  coord_fixed()
+  coord_fixed()+
+  ggtitle("Vaccine Improvement Index Values 1980-2019")
   
+#Work on way to loop through list of date frames to make plots automatically!
+
+
+
+
+
 
 #Just 2019 data
 df_2019 <- df %>% filter(year ==2019)
@@ -53,17 +54,19 @@ df_long_2019 <- df_simple_2019 %>% pivot_longer(!location,names_to = "component"
 
 #Make Heatmap of 2019 component variables
 ggplot(df_long_2019,aes(x=component,y=location, fill=Value))+
-  #add border white colour of line thickness 0.25
   geom_tile(colour="white", size=0.10)+
   #remove x and y axis labels
   labs(x="", y="")+
-  theme_minimal(base_size=3)+
-  theme(axis.text.x = element_text(angle = 45, hjust=1))
+  theme_minimal(base_size=6)+
+  theme(axis.text.x = element_text(angle = 45, hjust=1))+
+  #Creates diverging color scale
+  scale_fill_scico(palette = 'vikO',direction = -1)+
+  guides(fill=guide_colorbar(title="Component Value"))+
+  scale_y_discrete(expand=c(0,0))+
+  scale_x_discrete(expand=c(0,0),
+                   labels=c("cpi" = "Corruption Perception Index","dah_per_cap_ppp_mean" = "Development Assistance Per Person","ghes_per_the_mean" = "Government Health Spending per Total Health Spending", "haqi" = "HAQI", "imm_pop_perc"= "Immigrant Population (%)", "perc_skill_attend" = "Skilled Attendants at Birth", "perc_urban" = "Urbanicity (%)", "result" = "Improvement Index", "sdi" = "Socio-demographic Index", "the_per_cap_mean" = "Total Health Spending per Person"))+
   #keeps it squares
-  #coord_fixed()
-  # scale_fill_gradient(low = "#C6DBEF", high = "#08306B")+
-  # guides(fill=guide_legend(title="Index Value"))+
-  # scale_y_discrete(expand=c(0,0))+
-  # scale_x_discrete(expand=c(0,0))
+  #coord_fixed()+
+  ggtitle("Vaccine Improvement Index Component Values 2019")
 
-
+#Make heatmap of percent change between 2010 and 2019 for each component
